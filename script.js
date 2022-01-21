14d21a17fb54835cdd09f8a293058a3577773b69
smtandabuzo <sazi.mtandabuzo@metropolitan.co.za>
2022-01-21 14:55:42 +0200

    Progress

--- script.js
+++ script.js
@@ -1,21 +1,80 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullScreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+function showPlayIcon(){
+    playBtn.classList.replace('fa-pause','fa-play');
+    playBtn.setAttribute('title','Play');
+}
 
+function togglePlay() {
+    if(video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play','fa-pause');
+        playBtn.setAttribute('title','Pause');
+    } else {
+        video.pause();
+        showPlayIcon();
+    }
+}
+
+// On Video end, show play button
+video.addEventListener('ended',showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calculate display time format
+function displayTime(time){
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+// Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
 
+// Click to seek within the video
+function setProgress(e){
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+    console.log(newTime);
+}
 // Volume Controls --------------------------- //
 
-
+// Volume Bar
+function changeVolume(e){
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    console.log(volume);
+}
 
 // Change Playback Speed -------------------- //
 
 
 
 // Fullscreen ------------------------------- //
+
+// Event Listeners
+
+playBtn.addEventListener('click',togglePlay);
+video.addEventListener('click',togglePlay);
+video.addEventListener('timeupdate',updateProgress);
+video.addEventListener('canplay',updateProgress);
+progressRange.addEventListener('click',setProgress);
+volumeRange.addEventListener('click', changeVolume);
 
 
